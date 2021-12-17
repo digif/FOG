91a4f90dbaab729c0e0b354a472104fc5637aaff
aFranchon <3032fmartinage@gmail.com>
2021-12-17 22:08:31 +0100

    Fix Movements + Animations + Slide + Code cleaning + Refactoring

---
+++ Assets/FSLib/FSEvents/Listeners/IFSOneArgumentEventListener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Events;
+
+namespace FSLib.FSEvents.Listeners
+{
+    /// <summary>
+    /// Interface used for listeners to one arguments FSEventSO
+    /// </summary>
+    /// <typeparam name="T"> the type of the event to witch we register</typeparam>
+    /// <inheritdoc/>
+    
+    // ReSharper disable once InconsistentNaming
+    public abstract class IFsOneArgumentEventListener<T> : FsEventListener
+    {
+        #region Listener Handler
+
+        /// <summary>
+        /// Method called when FSEventSO raise this listener. It raise the <see cref="UnityEvent"/>
+        /// </summary>
+        /// <param name="value">the value to be pass to all subscribers</param>
+        protected abstract void Invoke(T value);
+
+        #endregion
+    }
+}
---
+++ Assets/FSLib/FSEvents/Listeners/IFSTwoArgumentEventListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Events;
+
+namespace FSLib.FSEvents.Listeners
+{
+    /// <summary>
+    /// Interface used for listeners to two arguments FSEventSO
+    /// </summary>
+    /// <typeparam name="T0"></typeparam>
+    /// <typeparam name="T1"></typeparam>
+    /// <inheritdoc/>
+    // ReSharper disable once InconsistentNaming
+    public abstract class IFsTwoArgumentEventListener<T0, T1> : FsEventListener
+    {
+        #region Listener Handler
+        
+        /// <summary>
+        /// Method called when FSEventSO raise this listener. It raise the <see cref="UnityEvent"/>
+        /// </summary>
+        /// <param name="valueOne">the second value to be pass to all subscribers</param>
+        /// <param name="valueTwo">the first value to be pass to all subscribers</param>
+        protected abstract void Invoke(T0 valueOne, T1 valueTwo);
+
+        #endregion
+    }
+}
